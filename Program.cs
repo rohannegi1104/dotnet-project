c2cc5439e0723172b87efd4b773f11022d142720
rohannegi1104 <rohansingh6814@gmail.com>
2025-01-28 17:52:01 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main()
     {
+   
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to the .NET application!");
+        Console.WriteLine("Thanks for running this application!");
     }
 }
